0906252172d1fc7bff0d9c7688ccd18b16774433
Faolan-GreenSquare <103235555+Faolan-GreenSquare@users.noreply.github.com>
2022-09-28 16:57:53 +0800

    files added

--- app/main.py
+++ app/main.py
@@ -1,48 +1,230 @@
-from flask import Flask, redirect, render_template, request
+from flask import Flask, render_template, request
+import folium
 
-from utils.types import PhysicalDevice
+from utils.types import *
 from utils.api import *
 
 app = Flask(__name__)
-data = get_physical_devices()
-debug_enabled = False
 
+debug_enabled = True
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
     physicalDevices = []
+    data = get_physical_devices()
+    if data is None:
+        return render_template('error_page.html')
     for i in range(len(data)):
         physicalDevices.append(PhysicalDevice(
             uid=data[i]['uid'],
             name=data[i]['name'],
             source_name=data[i]['source_name'],
-            last_seen=data[i]['last_seen']
+            last_seen=formatTimeStamp(data[i]['last_seen'])
         ))
     return render_template('physical_device_table.html', title='Physical Devices', physicalDevices=physicalDevices)
+    
 
-@app.route('/physical-device/<int:uid>', methods=['GET', 'POST'])
+@app.route('/physical-device/<int:uid>', methods=['GET'])
 def physical_device_form(uid):
-    data = get_physical_device(uid)
-    properties = format_json_property(data, 'properties')
-    mappings = get_device_mappings(uid)
-    mapping_formatted = format_json(mappings)
-    deviceName = data['name']
-    deviceLocation = data['location']
-    deviceLastSeen = data['last_seen']
-    title = 'Physical Device ' + str(uid) + ' - ' + str(deviceName)
-    if request.method == 'POST':
-        if request.form['create_mapping'] == 'Create Mapping':
-            newLogicalDevice = create_logical_device(data)
-            result = insert_device_mapping(uid, newLogicalDevice)
-            return redirect('/physical-device/'+str(uid))
-    elif request.method == 'GET':
-        pass
+    pd_data = get_physical_device(uid)
+    pd_data['location'] = formatLocationString(pd_data['location'])
+    pd_data['last_seen'] = formatTimeStamp(pd_data['last_seen'])
+    properties_formatted = format_json(pd_data['properties'])
+    sources = get_sources()
+    mappings = get_current_mappings(uid)
+    notes = get_physical_notes(uid)
+    currentDeviceMapping = []
+    deviceNotes = []
+    if mappings is not None:
+        currentDeviceMapping.append(DeviceMapping(
+            pd_uid=mappings['pd']['uid'],
+            pd_name=mappings['pd']['name'],
+            ld_uid=mappings['ld']['uid'],
+            ld_name=mappings['ld']['name'],
+            start_time=formatTimeStamp(mappings['start_time']),
+            end_time=formatTimeStamp(mappings['end_time'])))
+    if notes is not None:
+        for i in range(len(notes)):
+            deviceNotes.append(DeviceNote(
+                note=notes[i]['note'],
+                uid=notes[i]['uid'],
+                ts=formatTimeStamp(notes[i]['ts'])
+            )) 
+    
+    logicalDevices = []
+    ld_data = get_logical_devices()
+    for i in range(len(ld_data)):
+        logicalDevices.append(LogicalDevice(
+            uid=ld_data[i]['uid'],
+            name=ld_data[i]['name'],
+            location=formatLocationString(ld_data[i]['location']),
+            last_seen=formatTimeStamp(ld_data[i]['last_seen'])
+        ))
+
+    title = 'Physical Device ' + str(uid) + ' - ' + str(pd_data['name'])
     return render_template('physical_device_form.html',
                            title=title,
+                           pd_data=pd_data,
+                           ld_data=ld_data,
+                           sources=sources,
+                           properties=properties_formatted,
+                           currentMappings=currentDeviceMapping,
+                           deviceNotes=deviceNotes)
+
+
+@app.route('/logical-devices', methods=['GET'])
+def logical_device_table():
+    logicalDevices = []
+    ld_data = get_logical_devices()
+    for i in range(len(ld_data)):
+        logicalDevices.append(LogicalDevice(
+            uid=ld_data[i]['uid'],
+            name=ld_data[i]['name'],
+            location=formatLocationString(ld_data[i]['location']),
+            last_seen=formatTimeStamp(ld_data[i]['last_seen'])
+        ))
+    return render_template('logical_device_table.html', title='Logical Devices', logicalDevices=logicalDevices)
+
+
+@app.route('/logical-device/<int:uid>', methods=['GET'])
+def logical_device_form(uid):
+    ld_data = get_logical_device(uid)
+    properties_formatted = format_json(ld_data['properties'])
+    deviceName = ld_data['name']
+    deviceLocation = formatLocationString(ld_data['location'])
+    deviceLastSeen = formatTimeStamp(ld_data['last_seen'])
+    title = 'Logical Device ' + str(uid) + ' - ' + str(deviceName)
+    mappings = get_device_mappings(uid)
+    deviceMappings = []
+    if mappings is not None:
+        for i in range(len(mappings)):
+            deviceMappings.append(DeviceMapping(
+                pd_uid=mappings[i]['pd']['uid'],
+                pd_name=mappings[i]['pd']['name'],
+                ld_uid=mappings[i]['ld']['uid'],
+                ld_name=mappings[i]['ld']['name'],
+                start_time=formatTimeStamp(mappings[i]['start_time']),
+                end_time=formatTimeStamp(mappings[i]['end_time'])))
+    
+    physicalDevices = []
+    data = get_physical_devices()
+    for i in range(len(data)):
+        physicalDevices.append(PhysicalDevice(
+            uid=data[i]['uid'],
+            name=data[i]['name'],
+            source_name=data[i]['source_name'],
+            last_seen=formatTimeStamp(data[i]['last_seen'])
+        ))
+
+    return render_template('logical_device_form.html',
+                           title=title,
+                           ld_data=ld_data,
+                           pd_data=physicalDevices,
                            deviceLocation=deviceLocation,
                            deviceLastSeen=deviceLastSeen,
-                           properties=properties,
-                           mappings=mapping_formatted)
+                           properties=properties_formatted,
+                           deviceMappings=deviceMappings)
+
+
+@app.route('/map', methods=['GET'])
+def map():
+    center_map = folium.Map(location=[-32.2400951991083, 148.6324743348766], title = 'PhysicalDeviceMap', zoom_start=10)
+    #folium.Marker([-31.956194913619864, 115.85911692112582], popup="<i>Mt. Hood Meadows</i>", tooltip='click me').add_to(center_map)
+    data = get_physical_devices()
+    for i in range(len(data)):
+        if (data[i]['location'] is not None):
+            if(data[i]['source_name'] == "greenbrain"):
+                color = 'green'
+            elif(data[i]['source_name'] == "ttn"):
+                color = 'red'
+            else:
+                color = 'blue'
+            folium.Marker([data[i]['location']['lat'],data[i]['location']['long']],
+            popup = data[i]['uid'],
+            icon = folium.Icon(color=color, icon='cloud'),
+            tooltip = data[i]['name']).add_to(center_map)
+            
+    return center_map._repr_html_()
+    #center_map
+    #return render_template('map.html')
+
+
+@app.route('/create-mapping', methods=['GET'])
+def CreateMapping():
+    uid = request.args['uid']
+    data = get_physical_device(uid)
+    newLogicalDevice = create_logical_device(data)
+    result = insert_device_mapping(uid, newLogicalDevice)
+    return 'Success', 200
+
+@app.route('/create-note/<noteText>/<uid>', methods=['GET'])
+def CreateNote(noteText, uid):
+    result = insert_note(noteText, uid)
+    return 'Success', 200
+
+
+@app.route('/delete-note/<noteUID>', methods=['DELETE'])
+def DeleteNote(noteUID):
+    result = delete_note(noteUID)
+    return 'Success', 200
+
+
+@app.route('/edit-note/<noteText>/<uid>', methods=['PATCH'])
+def EditNote(noteText, uid):
+    result = edit_note(noteText, uid)
+    return 'Success', 200
+
+
+@app.route('/update-physical-device', methods=['GET'])
+def UpdatePhysicalDevice():
+    if request.args['form_location'] != None and request.args['form_location'] != '' and request.args['form_location'] != 'None':
+        location = request.args['form_location'].split(',')
+        locationJson = {
+            "lat": location[0].replace(' ', ''),
+            "long": location[1].replace(' ', ''),
+        }
+    else:
+        locationJson = None
+
+    update_physical_device(request.args['form_uid'], request.args['form_name'], locationJson)
+    return 'Success', 200
+
+
+@app.route('/update-mappings', methods=['GET'])
+def UpdateMappings():
+    insert_device_mapping(request.args['physicalDevice_mapping'], request.args['logicalDevice_mapping'])
+    return 'Success', 200
+
+
+@app.route('/update-logical-device', methods=['GET'])
+def UpdateLogicalDevice():
+    if request.args['form_location'] != None and request.args['form_location'] != '' and request.args['form_location'] != 'None':
+        location = request.args['form_location'].split(',')
+        locationJson = {
+            "lat": location[0].replace(' ', ''),
+            "long": location[1].replace(' ', ''),
+        }
+    else:
+        locationJson = None
+
+    update_logical_device(request.args['form_uid'], request.args['form_name'], locationJson)
+    return 'Success', 200
+
+def formatTimeStamp(unformattedTime):
+    if unformattedTime:
+        formattedLastSeen = unformattedTime[0:19]
+        formattedLastSeen = formattedLastSeen.replace('T', ' ')
+        return formattedLastSeen
+
+def formatLocationString(locationJson):
+    if locationJson is not None:
+        formattedLocation = str(locationJson['lat'])
+        formattedLocation += ', '
+        formattedLocation += str(locationJson['long'])
+    else:
+        formattedLocation = None
+    return formattedLocation
 
 if __name__ == '__main__':
     app.run(debug=debug_enabled, port='5000', host='0.0.0.0')
+    #app.run(debug=True)
--- app/utils/api.py
+++ app/utils/api.py
@@ -1,75 +1,179 @@
-import json
-import requests
+import json, requests
+import os
 from datetime import datetime
 
-physical_link_all = 'https://staging.farmdecisiontech.net.au/broker/api/physical/devices/?include_properties=false'
-physical_link_uid = 'https://staging.farmdecisiontech.net.au/broker/api/physical/devices/'
-logical_link_insert = 'https://staging.farmdecisiontech.net.au/broker/api/logical/devices/'
-mapping_link_all = 'https://staging.farmdecisiontech.net.au/broker/api/mappings/logical/all/'
-mapping_link_current = 'https://staging.farmdecisiontech.net.au/broker/api/mappings/physical/current/'
-mapping_link_insert = 'https://staging.farmdecisiontech.net.au/broker/api/mappings/'
-headers = {"Authorization": "Bearer bad_token"}
+
+#end_point = os.environ['end_point']
+#bearer_token = os.environ['bearer_token']
+
+end_point = 'https://staging.farmdecisiontech.net.au'
+bearer_token = 'bad_token'
+    
+
+# Physical Device Links
+physical_link_all = end_point + '/broker/api/physical/devices/?include_properties=false'
+physical_link_notes = end_point + '/broker/api/physical/devices/notes/'
+physical_link_uid = end_point + '/broker/api/physical/devices/'
+physical_link_update = end_point + '/broker/api/physical/devices/'
+
+# Logical Device Links
+logical_link_all = end_point + '/broker/api/logical/devices/?include_properties=false'
+logical_link_uid = end_point + '/broker/api/logical/devices/'
+logical_link_insert = end_point + '/broker/api/logical/devices/'
+logical_link_update = end_point + '/broker/api/logical/devices/'
+
+# Mapping Device Links
+physical_link_unmapped = end_point + '/broker/api/physical/devices/unmapped/'
+physical_link_endmapping = end_point + '/broker/api/mappings/physical/end/'
+logical_link_endmapping = end_point + '/broker/api/mappings/logical/end/'
+mapping_link_all = end_point + '/broker/api/mappings/logical/all/'
+mapping_link_current = end_point + '/broker/api/mappings/physical/current/'
+mapping_link_insert = end_point + '/broker/api/mappings/'
+
+# Other links
+note_link_insert = end_point + '/broker/api/physical/devices/notes/'
+delete_note_url = end_point + '/broker/api/physical/devices/notes/'
+sources_link_all = end_point + '/broker/api/physical/sources/'
+
+headers = {"Authorization": "Bearer " + bearer_token}
+
+def get_sources():
+    response = requests.get(sources_link_all, headers=headers)
+    if response.status_code == 200:
+        return response.json()    
 
 def get_physical_devices():
     response = requests.get(physical_link_all, headers=headers)
-    return response.json()
+    if response.status_code == 200:
+        return response.json()
+
+def get_physical_notes(uid):
+    response = requests.get(physical_link_notes + str(uid), headers=headers)
+    if response.status_code == 200:
+        return response.json()
+
+def get_logical_devices():
+    response = requests.get(logical_link_all, headers=headers)
+    if response.status_code == 200:
+        return response.json()
+
+def get_physical_unmapped():
+    response = requests.get(physical_link_unmapped, headers=headers)
+    if response.status_code == 200:
+        return response.json()
 
 def get_physical_device(uid):
     response = requests.get(physical_link_uid + str(uid), headers=headers)
-    return response.json()
+    if response.status_code == 200:
+        return response.json()
+
+def get_logical_device(uid):
+    response = requests.get(logical_link_uid + str(uid), headers=headers)
+    if response.status_code == 200:
+        return response.json()    
 
 def get_device_mappings(uid):
+    response = requests.get(mapping_link_all + str(uid), headers=headers)
+    if response.status_code == 200:
+        return response.json()    
+
+def get_current_mappings(uid):
     response = requests.get(mapping_link_current + str(uid), headers=headers)
-    return response.json()
+    if response.status_code == 200:
+        return response.json()    
+    
+def update_physical_device(uid, name, location):
+    device = get_physical_device(uid)
+    device['name'] = name
+    device['location'] = location
+    print(format_json(device))
+    response = requests.patch(physical_link_update, headers=headers, json=device)
+    if response.status_code == 200:
+        return response.json()
+
+def update_logical_device(uid, name, location):
+    device = get_logical_device(uid)
+    device['name'] = name
+    device['location'] = location
+    print(format_json(device))
+    response = requests.patch(logical_link_update, headers=headers, json=device)
+    if response.status_code == 200:
+        return response.json()
+
+def end_logical_mapping(uid):
+    url = logical_link_endmapping + uid
+    requests.patch(url, headers=headers)
+
+def end_physical_mapping(uid):
+    url = physical_link_endmapping + uid
+    requests.patch(url, headers=headers)
 
 def create_logical_device(data):
     timeObject = datetime.now()
-    print(timeObject)
-    print(timeObject.tzname())
-
     logicalJson = {
         "uid": 0,
         "name": data['name'],
         "location": data['location'],
         "last_seen": str(timeObject),
-        "properties": {}
+        "properties": data['properties']
         }
     response = requests.post(logical_link_insert, json=logicalJson, headers=headers)
     logical_device = response.json()
     return logical_device['uid']
 
+def delete_note(uid):
+    baseUrl = delete_note_url + str(uid)
+    response= requests.delete(baseUrl, headers=headers)
+    return response.json()
+
+
 def insert_device_mapping(physicalUid, logicalUid):
+    logicalDevice = get_logical_device(logicalUid)
+    physicalDevice = get_physical_device(physicalUid)
+    timeObject = datetime.now()
+    end_physical_mapping(physicalUid)
     mappingJson = {
         "pd": {
-            "uid": physicalUid,
-            "source_name": "string",
-            "name": "string",
-            "location": {
-            "lat": 0,
-            "long": 0
-            },
-            "last_seen": "2022-05-18T09:50:01.928Z",
-            "source_ids": {},
-            "properties": {}
+            "uid": physicalDevice['uid'],
+            "source_name": physicalDevice['source_name'],
+            "name": physicalDevice['name'],
+            "location": physicalDevice['location'],
+            "last_seen": physicalDevice['last_seen'],
+            "source_ids": physicalDevice['source_ids'],
+            "properties": physicalDevice['properties']
         },
         "ld": {
-            "uid": logicalUid,
-            "name": "string",
-            "location": {
-            "lat": 0,
-            "long": 0
-            },
-            "last_seen": "2022-05-18T09:50:01.928Z",
-            "properties": {}
+            "uid": logicalDevice['uid'],
+            "name": logicalDevice['name'],
+            "location": logicalDevice['location'],
+            "last_seen": logicalDevice['last_seen'],
+            "properties": logicalDevice['properties']
         },
-        "start_time": "2022-05-18T09:50:01.928Z",
-        "end_time": "2022-05-18T09:50:01.928Z"
-        }
+        "start_time": str(timeObject),
+        "end_time": str(timeObject)
+    }
     response = requests.post(mapping_link_insert, json=mappingJson, headers=headers)
+    return response.json()
+
+def insert_note(noteText, uid):
+    noteJson = {
+        "note" : noteText
+        }
+    baseUrl = note_link_insert + uid
+    response = requests.post(baseUrl, json=noteJson, headers=headers)
+    return response.json()
+
+def edit_note(noteText, uid):
+    timeObject = datetime.now()
+    noteJson = {
+        "uid" : int(uid),
+        "ts": str(timeObject),
+        "note" : str(noteText)
+        }
+    baseUrl = note_link_insert
+    response = requests.patch(baseUrl, json=noteJson, headers=headers)
+    
     return response.json()
 
 def format_json(data):
     return (json.dumps(data, indent=4, sort_keys=True))
-
-def format_json_property(data, property):
-    return (json.dumps(data[property], indent=4, sort_keys=True))
